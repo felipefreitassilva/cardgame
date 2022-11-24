ebf7bff557c950571b6c04a869b9706d714049fc
Felipe Freitas Silva <75501741+felipefreitassilva@users.noreply.github.com>
2022-11-24 02:07:38 -0300

    Created Pokemon Basics

---
+++ Type.java
@@ -0,0 +1,20 @@
+public enum Type {
+    NORMAL,
+    FIGHT,
+    FLYING,
+    POISON,
+    GROUND,
+    ROCK,
+    BUG,
+    GHOST,
+    STEEL,
+    FIRE,
+    WATER,
+    GRASS,
+    ELECTRIC,
+    PSYCHIC,
+    ICE,
+    DRAGON,
+    DARK,
+    FAIRY
+}
